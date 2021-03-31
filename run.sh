e1fc42e9a1a7ad2817f1ebde3129ab14ee48bd7c
Shirish Pal <Shirish@Shirishs-MacBook-Pro.local>
2021-03-30 18:37:30 -0700

    path update

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 docker rm -f tlspack_node_service
 docker network disconnect --force host tlspack_node_service
 docker run --network=host --name tlspack_node_service --volume="$1:/rundir" -it -d --rm tlspack/node:latest /bin/bash
-docker exec -d -w /rundir tlspack_node_service python3 -m NodeService "$1"
+docker exec -d -w / tlspack_node_service python3 -m rundir.NodeService "$1"
 
