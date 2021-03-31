7eb32ab4ed1a5196480f6720a0318c98f9317885
Shirish Pal <Shirish@Shirishs-MacBook-Pro.local>
2021-03-30 19:27:41 -0700

    service name

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker rm -f tlspack_node_service
-docker network disconnect --force host tlspack_node_service
-docker run --network=host --name tlspack_node_service --volume="$1:/rundir" -it -d --rm tlspack/node:latest /bin/bash
-docker exec -d -w / tlspack_node_service python3 -m rundir.NodeService "$1"
+docker rm -f TlspackNodeService
+docker network disconnect --force host TlspackNodeService
+docker run --network=host --name TlspackNodeService --volume="$1:/rundir" -it -d --rm tlspack/node:latest /bin/bash
+docker exec -d -w / TlspackNodeService python3 -m rundir.NodeService "$1"
 
