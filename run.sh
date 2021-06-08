eeb2ac2faee87560d771c4a22d8933ec605ab8a8
Shirish Pal <shirish.pal@gigamon.com>
2021-06-07 23:15:45 -0700

    refactor

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker rm -f TlspackNodeService
 docker network disconnect --force host TlspackNodeService
-docker run --network=host --name TlspackNodeService --volume="$1:/rundir" -it -d --rm tlspack/node:latest /bin/bash
+docker run --network=host --name TlspackNodeService --volume="$1:/rundir" --volume="$2:/uidir" --volume="$3:/plugindir" -it -d --rm tlspack/node:latest /bin/bash
 sleep 1
 docker exec -d -w / TlspackNodeService python3 -m rundir.NodeService "$1"
 sleep 1
